c90f5413db46ad64b5c1965b54ba254c5505ed2c
OM Parte <154506713+OMParte-10@users.noreply.github.com>
2024-12-12 21:37:36 +0530

    Update v4software.py

--- v4software.py
+++ v4software.py
@@ -21,7 +21,7 @@ y = (a * (x ** 2)) + (b * x) + c
 print("Value of y:", y)
 
 # Read input from a file
-with open('input.txt.txt', 'r') as file:
+with open('input_x.txt', 'r') as file:
     a = int(file.readline().strip())
     b = int(file.readline().strip())
     c = int(file.readline().strip())
